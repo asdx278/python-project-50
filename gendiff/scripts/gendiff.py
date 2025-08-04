81c6d160d2b05beec5fbf9b940478a3712d2e787
Albert Shubin <albert.shubin@gmail.com>
2025-08-04 23:42:34 +0400

    add argument

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -5,6 +5,7 @@ def main():
     "files and shows a difference.")
     parser.add_argument("first_file")
     parser.add_argument("second_file")
+    parser.add_argument("-f", "--format", help="set format of output")
     args = parser.parse_args()
 
 if __name__ == "__main__":
