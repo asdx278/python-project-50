b5cacfdd6248503aacfe2c3a898049952c8202e7
Albert Shubin <albert.shubin@gmail.com>
2025-08-04 17:05:23 +0400

    add gendiff module, help info

---
+++ gendiff/scripts/gendiff.py
@@ -0,0 +1,12 @@
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser(description="Compares two configuration " \
+    "files and shows a difference.")
+    parser.add_argument("first_file")
+    parser.add_argument("second_file")
+    args = parser.parse_args()
+    print(f"Привет, {args.first_file}!")
+
+if __name__ == "__main__":
+    main()
