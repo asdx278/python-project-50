efd9853038e1f6b1f6ec5a8b205f48212ea6d3b7
Albert Shubin <albert.shubin@gmail.com>
2025-08-04 17:08:45 +0400

    fix main

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -6,7 +6,6 @@ def main():
     parser.add_argument("first_file")
     parser.add_argument("second_file")
     args = parser.parse_args()
-    print(f"Привет, {args.first_file}!")
 
 if __name__ == "__main__":
     main()
